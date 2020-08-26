fa03fd11e5dc49c427930d71dbfd9f4aa719269b
zhangyang <zhangyang@lingkedsee.com>
2020-08-26 17:57:26 +0800

    测试vant组件成功

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import { Row, Col, Icon, Cell, CellGroup } from 'vant';
+import { Button,Row, Col, Icon, Cell, CellGroup } from 'vant';
 
 @Options({
   components: {
     HelloWorld,
+    [Button.name]:Button
   },
 })
 export default class Home extends Vue {}
